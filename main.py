d3dc65d9278378d5b3f1acafbdb9d0f92996dd31
Thomas Jensen <thomas@chillipot.net>
2020-04-30 23:35:40 +0000

    cosmetics and added "and finally" before last number

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 language = "python3"
 # little counter program
 
-print ("Hello World")
-num = int(input ("\n let's count to ...: "))
+print ("Hello World of counting")
+num = int(input ("\n let's count to : "))
 n = 0
 while n < num:
  n += 1
  print (f" {n}")
-print ("\n that's all folks, thanks for watching")
+ if n == (num - 1): print ("and finally")
+print ("\n That's all folks, thanks for watching")
