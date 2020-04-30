710ba3c7ece6b09bbbd64c0ef25c5a15fd78d928
Thomas Jensen <thomas@chillipot.net>
2020-04-30 23:25:03 +0000

    input of num as well as count logic with print

---
+++ main.py
@@ -0,0 +1,10 @@
+language = "python3"
+# little counter program
+
+print ("Hello World")
+num = int(input ("\n let's count to ...: "))
+n = 0
+while n < num:
+ n += 1
+ print (f" {n}")
+print ("\n that's all folks, thanks for watching")
